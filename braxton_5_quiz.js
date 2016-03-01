72f1b91f1eb42c56f4f94c987d49ee00755bda90
mbraxton6099 <mbraxton6099@gmail.com>
2016-02-29 20:15:34 -0600

    oops

---
+++ braxton_5_quiz.js
@@ -0,0 +1,34 @@
+var a1 = prompt("Have you bought People Pills before?").toLowerCase();
+/*var a2 = prompt("Can you buy a Beyonce's Height pill?").toLowerCase();
+var a3 = prompt("Can you use Euros to buy People Pills?").toLowerCase();
+var a4 = prompt("Is there a Obama Pill?").toLowerCase();
+var a5 = prompt("Is there an Donalds Duck edition?").toLowerCase();
+var a6 = prompt("In the Delux pack can you buy everything?").toLowerCase();
+var a7 = prompt("Is there a waiting time?").toLowerCase();
+var a8 = prompt("What's the way to get rid of Beyonce's Hair?", "Reverse PP or Trump's Hair PP").toLowerCase();
+var a9 = prompt("How can many comes in a pequeno pack?", "7 or 14").toLowerCase();
+var a10 = prompt("Do you want to buy people pills now?").toLowerCase();*/
+
+if (a1 == "yes" || "no") {
+    var a2 = prompt("Can you buy a Beyonce's Height pill?").toLowerCase();
+} if (a2 == "no") {
+    var a3 = prompt("Can you use Euros to buy People Pills?").toLowerCase();
+} if (a3 == "no") {
+    var a4 = prompt("Is there a Obama Pill?").toLowerCase();
+} if (a4 == "no") {
+    var a5 = prompt("Is there an Donalds Duck edition?").toLowerCase();
+} if (a5 == "yes") {
+    var a6 = prompt("In the Delux pack can you buy everything?").toLowerCase();
+} if (a6 == "yes") {
+    var a7 = prompt("Is there a duration time?").toLowerCase();
+} if (a7 == "no") {
+    var a8 = prompt("What's the way to get rid of Beyonce's Hair?", "Reverse PP or Trump's Hair PP").toLowerCase();
+} if (a8 == "trump's hair pp") {
+    var a9 = prompt("How can many comes in a pequeno pack?", "7 or 14").toLowerCase();
+} if (a9 == "7") {
+    var a10 = prompt("Do you want to buy people pills now?").toLowerCase();
+} if (a10 == "yes") {
+    document.write("You Win");
+} else {
+    document.write("Try again");
+} 
